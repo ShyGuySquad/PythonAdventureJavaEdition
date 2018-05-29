ca2145a3e5216a936f2782b5d3a4eff7e20fdcdf
Jacob Merrill <jrmerrill2004@gmail.com>
2018-05-29 13:02:29 -0700

    Update PyAdventJavaEd.java

--- PyAdventJavaEd.java
+++ PyAdventJavaEd.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class PyAdventJavaEd {
   public static void main(String[] args) {
     System.out.println("PYTHON ADVENTURE");
     System.out.println("Java Edition");
-    System.out.println("Press [ENTER]");
+    Scanner replyIn = new Scanner(System.in);
+    // to use keyboard input, simply type:
+    String reply = replyIn.nextLine();
+    // now reply will be whatever the user typed in before pressing enter.
   }
 }
